635f20d6ca3d140e10e5512c13104c1164dac837
mwaskagi <gituiku.mwangi@gmail.com>
2023-03-12 00:40:40 -0800

    Test Base

---
+++ models/base_model.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Defines the BaseModel class."""
+
+class BaseModel:
+    """Represents the BaseModel of the HBnB project."""
+
+    def __init__(self, *args, **kwargs):
+        """Initialize a new BaseModel.
+
+        Args:
+            *args (any): Unused.
+            **kwargs (dict): Key/value pairs of attributes.
+        """
